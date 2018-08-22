4495714e125b0692aa45158ecb1ebfb25836a7fe
jeanetteobr <jeanette.obr@gmail.com>
2018-08-21 22:29:31 -0400

    added color scheme, modified layout

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let avatarDiv = document.getElementById('avatar')
 
 function requestData () {
   request.get('https://api.github.com/users/jeanetteobr')
-    .auth('jeanetteobr', '14444c7e668f320de612f16af9de03d3a658d27e')
+    .auth('jeanetteobr', 'bf6d51e30a19ab9abac771efa254819b2b1f4f76')
     .then(response => {
       console.log(response)
       displayContactResults(response.body)
@@ -23,9 +23,9 @@ function displayContactResults (results) {
   let outputContact = `
    <div class="biz-card">
    <h1>Contact</h1>
-  <p>Name: ${results.name}</p>
-  <p>Github: ${results.html_url}</p>
-  <p>Website: ${results.blog}</p>
+  <p><span id="key">Name</span> ${results.name}</p>
+  <p><span id="key">Github</span> ${results.html_url}</p>
+  <p><span id="key">Website</span> ${results.blog}</p>
    </div>
    `
   contactDiv.innerHTML = outputContact
@@ -43,8 +43,8 @@ function displayBioResults (results) {
 
 function displayPhotoResults (results) {
   let outputImg = `
-  <div class="biz-card">
-  <img src="${results.avatar_url}" alt="Jeanette O'Brien avatar"
+  <div class="biz-card__avatar">
+  <img src="https://avatars0.githubusercontent.com/u/32466511?v=3" alt="Jeanette O'Brien avatar"
   </div>
   `
   avatarDiv.innerHTML = outputImg
